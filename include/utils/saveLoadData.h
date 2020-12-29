bae49e128b4f8614bd30b38207c4dc5d495347b3
kevinmalaverr <kevinmalaverr@gmail.com>
2020-12-29 14:54:51 -0500

    initial structure

---
+++ include/utils/saveLoadData.h
@@ -0,0 +1,12 @@
+#include <string>
+
+#ifndef SAVELOADDATA_H_INCLUDE
+#define SAVELOADDATA_H_INCLUDE
+
+
+using namespace std;
+
+void saveData(string data);
+string loadData();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "utils/saveLoadData.h"
+
+using namespace std;
+
+
+void drawMap(int posX,int posY,char gameMap[5][5]){
+  for(int i=0;i<5;i++){
+    for(int j=0;j<5;j++){
+      if(posX==j && posY==i){
+        cout<<"H";
+      }
+      else{
+	cout<<gameMap[i][j];
+      }
+    } 
+    cout<<endl;
+  }
+}
+
+
+
+// int main(){
+
+//   int posX=0;
+//   int posY=0;
+//   char map[5][5]={{'0','0','0','0','0'},
+//                   {'0','0','0','0','0'},
+//                   {'0','0','0','0','0'},
+//                   {'0','0','0','0','0'},
+//                   {'0','0','0','0','0'}};
+//   char teclado;
+//   bool gameOver= false;
+
+//   drawMap(posX,posY,map);
+//   while(!gameOver){
+//   cin>>teclado;
+//   switch (teclado)
+//   {
+//   case 'a':
+//        posX-=1;
+//        break;
+//   case 'd':
+// 	 posX+=1;
+//        break;
+//   case 'w':
+//          posY-=1;
+//        break;
+//   case 's':
+//          posY+=1;
+//        break;
+//   case 'p':
+// 	 gameOver=true;
+//   default:
+//       break;
+//   }
+//   drawMap(posX,posY,map);
+//   }
+ 
+//  return 0;
+// }
+
+int main()
+{
+  string a = "dfdfdf";
+  saveData(a);
+
+  cout << a << endl;
+
+  return 0;
+}
